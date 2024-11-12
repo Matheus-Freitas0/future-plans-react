5dc9894a6d45239555de8aad76a231ebff63c391
MatheusFreitas <moncaomatheus@gmail.com>
2024-11-12 14:26:39 -0300

    Updated Header component with new CSS styles and navigation links, and added global CSS styles to index.css

--- src/components/pageMovieCatalog/Header.jsx
+++ src/components/pageMovieCatalog/Header.jsx
@@ -1,13 +1,43 @@
-import './Header.css'
+import { useNavigate } from "react-router-dom";
+import Logo from "../../assets/logoMovie.png";
+import React from "react";
+import "./Header.css";
 
 function Header() {
-    return (
-        <div className="header">
-            <nav>
-                <h1>Header</h1>
-            </nav>
-        </div>
-    )
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate("/");
+  };
+
+  const goRegister = () => {
+    navigate("/register");
+  };
+
+  const goFavorites = () => {
+    navigate("/movie/details");
+  };
+
+  return (
+    <div className="header">
+      <nav>
+        <img src={Logo} alt="Logo" className="navbar-logo-img" onClick={goHome} />
+
+        {/* Links de navegação */}
+        <ul className="navbar-links">
+          <li>
+            <button className="navbar-link" onClick={goHome}>Home</button>
+          </li>
+          <li>
+            <button className="navbar-link" onClick={goFavorites}>Favorites</button>
+          </li>
+          <li>
+            <button className="navbar-link" onClick={goRegister}>Register Movie</button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
