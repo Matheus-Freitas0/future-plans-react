3e78811aa6c3d8517c43ff09728c34b9ae407ddf
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-12 18:17:33 -0300

    Added 5 new div elements with h1 tags containing 'foto1' to 'foto5' text inside the 'important-plans' div.

--- src/components/HomePage/ImportantPlans.jsx
+++ src/components/HomePage/ImportantPlans.jsx
@@ -4,7 +4,21 @@ function ImportantPlans() {
         <div className="important-plans">
             <h1>Main plans</h1>
             <div>
-
+                <div>
+                    <h1>foto1</h1>
+                </div>
+                <div>
+                    <h1>foto2</h1>
+                </div>
+                <div>
+                    <h1>foto3</h1>
+                </div>
+                <div>
+                    <h1>foto4</h1>
+                </div>
+                <div>
+                    <h1>foto5</h1>
+                </div>
             </div>
         </div>
     )
