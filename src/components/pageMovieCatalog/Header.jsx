ea296f1b97d47f2dd9e10dde6a2a027162b57045
MatheusFreitas <moncaomatheus@gmail.com>
2024-11-12 12:57:26 -0300

    Updated index.html with new title, favicon, and Google Fonts links; added font family to index.css; and refactored MovieCatalog.jsx to include a Header component.

---
+++ src/components/pageMovieCatalog/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <nav>
+                <h1>Header</h1>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
--- src/pages/MovieCatalog.jsx
+++ src/pages/MovieCatalog.jsx
@@ -1,7 +1,9 @@
+import Header from "../components/pageMovieCatalog/Header"
+
 function MovieCatalog() {
     return (
-        <div className="movie-catalog">
-            <h1>Movie Catalog</h1>
+        <div >
+            <Header />
         </div>
     )
 }
